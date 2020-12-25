9f24d5dc3f97d875b268fece47b0d2ec4f78f067
shogun786 <skumarc2003@gmail.com>
2020-12-25 01:14:27 -0800

    find first duplicate

--- arrays/findFirstDuplicate.cpp
+++ arrays/findFirstDuplicate.cpp
@@ -1,27 +1,32 @@
 /*
  * input: [2, 1, 5, 2, 3, 3, 4]
  * first duplicate result is 2
+ * find whose first duplicate value has the the minimum index
  *
  * other solutions could be to use a hashtable
  */
 
+//[9, 13, 6, 2, 3, 5, 5, 5, 3, 2, 2, 2, 2, 4, 3]
+//result should be 5 not 2
 int findFirstDuplicate(vector<int> &array){
-	for(int i=0; i<array.size()-1; i++){
+  int minIdx=INT_MAX;
+	for(int i=0; i<array.size(); i++){
 		for(int j=i+1; j<array.size(); j++){
-			if(array[i]==araay[j])
-				return array[i];
+			if(array[i]==araay[j]){
+         minIdx=min(minIdx,j);
+      }
 		}
 	}
-	return -1;
+	return minIdx==INT_MAX?-1:array[minIdx];;
 }
 
 int firstDuplicateValue(vector<int> array) {
 	for(int i=0; i<array.size(); i++){
-		int v=array[i]<0?-array[i]:array[i];
+		int v=abs(array[i]);
 		if(array[v-1]<0){
 			return v;
 		}else{
-			array[v-1]=-array[v-1];
+			array[v-1]*=-1;
 		}
 	}
 	return -1;
