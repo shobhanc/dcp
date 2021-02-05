54cbff36b09057de0fb1ffa03e36494dcfde8b81
shogun786 <skumarc2003@gmail.com>
2021-02-05 00:02:35 -0800

    indent

--- trees/findSuccessor.cpp
+++ trees/findSuccessor.cpp
@@ -7,34 +7,34 @@
 // This is an input class. Do not edit.
 // time O(n) and space O(n)
 class BinaryTree {
-public:
-  int value;
-  BinaryTree *left = nullptr;
-  BinaryTree *right = nullptr;
-  BinaryTree *parent = nullptr;
+	public:
+		int value;
+		BinaryTree *left = nullptr;
+		BinaryTree *right = nullptr;
+		BinaryTree *parent = nullptr;
 
-  BinaryTree(int value) { this->value = value; }
+		BinaryTree(int value) { this->value = value; }
 };
 
 void inorder(BinaryTree *tree, vector<BinaryTree*> &v){
-    if(tree==NULL){
-       return;
-		}
+	if(tree==NULL){
+		return;
+	}
 	inorder(tree->left, v);
-	  v.push_back(tree);
-	  inorder(tree->right, v);
+	v.push_back(tree);
+	inorder(tree->right, v);
 }
 
 //do inorder and capture in a vector and secrch for given node and return the next in the list
 BinaryTree *findSuccessor(BinaryTree *tree, BinaryTree *node) {
-  // Write your code here.
+	// Write your code here.
 	vector<BinaryTree*> v;
 	inorder(tree, v);
 	int i=0;
 	for(; i<v.size();i++){
-     if(v[i]==node){
-        break;
-		 }
+		if(v[i]==node){
+			break;
+		}
 	}
 	return ((i+1)<v.size())?v[i+1]:nullptr;
 }
@@ -53,8 +53,8 @@ BinaryTree *findSuccessor(BinaryTree *tree, BinaryTree *node) {
 //     So go to A(B's parent) and and A's right C is not B. So return B's parent.
 BinaryTree* findRightMostParent(BinaryTree* cur){
 	while(cur->parent!= NULL && 
-			 cur->parent->right == cur){
-       cur = cur->parent;
+			cur->parent->right == cur){
+		cur = cur->parent;
 	}
 	return cur->parent;
 }
@@ -69,21 +69,19 @@ BinaryTree* findRightMostParent(BinaryTree* cur){
 //
 //B's successor is the left most, in this case D is the succesor
 BinaryTree* findLeftMost(BinaryTree *cur){
-	 while(cur->left){
-      cur = cur->left;
-	 }
-	 return cur;
+	while(cur->left){
+		cur = cur->left;
+	}
+	return cur;
 }
 
 
 BinaryTree *findSuccessor(BinaryTree *tree, BinaryTree *node) {
-  // Write your code here.
+	// Write your code here.
 	//go right and find the left most
 	if(node->right){
-     return findLeftMost(node->right);
+		return findLeftMost(node->right);
 	}
 	//find parent of rightmost parent
 	return findRightMostParent(node);
 }
-
-
